41d860146fbd4a260e9fbe79b76620b2b9fbcd58
Samuel <samuel.iza@outlook.com>
2025-07-03 17:16:37 -0600

    Implementacion clase ZonaFitApplication

--- src/main/java/com/zona_fit/ZonaFitApplication.java
+++ src/main/java/com/zona_fit/ZonaFitApplication.java
@@ -1,13 +1,32 @@
 package com.zona_fit;
 
+import com.zona_fit.servicio.IClienteServicio;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class ZonaFitApplication {
+public class ZonaFitApplication implements CommandLineRunner {
+
+	@Autowired
+	private IClienteServicio clienteServicio;
+
+	private static final Logger logger = LoggerFactory.getLogger(ZonaFitApplication.class);
+
 
 	public static void main(String[] args) {
+		logger.info("Iniciando aplicacion");
+		//Levantar la fabrica de Spring
 		SpringApplication.run(ZonaFitApplication.class, args);
+		logger.info("Aplicacion finalizada");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("*** Aplicacion Zona Fit ***");
+	}
 }
