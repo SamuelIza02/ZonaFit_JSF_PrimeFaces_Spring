8403b5b1ed7da2a6c700cdbaefb3e9d74df1858d
Samuel <samuel.iza@outlook.com>
2025-07-04 13:35:19 -0600

    Creacion de GUI design

---
+++ src/main/java/com/zona_fit/gui/ZonaFitForma.java
@@ -0,0 +1,20 @@
+package com.zona_fit.gui;
+
+import com.zona_fit.servicio.ClienteServicio;
+import com.zona_fit.servicio.IClienteServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+
+@Component
+public class ZonaFitForma extends JFrame{
+    private JPanel panelPrincipal;
+    IClienteServicio clienteServicio;
+
+    @Autowired
+    public ZonaFitForma(ClienteServicio clienteServicio){
+        this.clienteServicio = clienteServicio;
+
+    }
+}
