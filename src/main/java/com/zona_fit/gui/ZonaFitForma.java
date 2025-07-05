a197cc63081e311316ce17de69d7be1a99ff6bf5
Samuel <samuel.iza@outlook.com>
2025-07-04 20:08:07 -0600

    Creacion de metodo createUIComponents clase ZonaFitForma

--- src/main/java/com/zona_fit/gui/ZonaFitForma.java
+++ src/main/java/com/zona_fit/gui/ZonaFitForma.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 @Component
 public class ZonaFitForma extends JFrame{
     private JPanel panelPrincipal;
     private JTable clientesTabla;
     IClienteServicio clienteServicio;
+    private DefaultTableModel tablaModeloClientes;
 
     @Autowired
     public ZonaFitForma(ClienteServicio clienteServicio){
@@ -27,4 +29,11 @@ public class ZonaFitForma extends JFrame{
     }
 
 
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        this.tablaModeloClientes = new DefaultTableModel(0, 4);
+        String[] cabeceros = {"ID", "Nombre", "Apellido", "Membresia"};
+        this.tablaModeloClientes.setColumnIdentifiers(cabeceros);
+        this.clientesTabla = new JTable(tablaModeloClientes);
+    }
 }
