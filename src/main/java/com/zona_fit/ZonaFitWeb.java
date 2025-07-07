0c34243a6f205d7cf1a04dcaf7fc713cdcd2fe0e
Samuel <samuel.iza@outlook.com>
2025-07-06 19:23:20 -0600

    Creacion index.xhtml

--- src/main/java/com/zona_fit/ZonaFitWeb.java
+++ src/main/java/com/zona_fit/ZonaFitWeb.java
@@ -1,4 +1,11 @@
 package com.zona_fit;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ZonaFitWeb {
+    public static void main(String[] args) {
+        SpringApplication.run(ZonaFitWeb.class, args);
+    }
 }
